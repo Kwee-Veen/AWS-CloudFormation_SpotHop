49b8296a8a8e9c9f8fa42e9ef27258b839f7794f
Caoimhín Arnott <118535981+Kwee-Veen@users.noreply.github.com>
2024-04-18 16:13:40 +0100

    Update mem.sh

--- mem.sh
+++ mem.sh
@@ -10,5 +10,5 @@ IO_WAIT=$(iostat | awk 'NR==4 {print $5}')
 
 aws cloudwatch put-metric-data --metric-name memory-usage --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $USEDMEMORY
 aws cloudwatch put-metric-data --metric-name Tcp_connections --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $TCP_CONN
-aws cloudwatch put-metric-data --metric-name TCP_connection_on_port_80 --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $TCP_CONN_PORT_80
+aws cloudwatch put-metric-data --metric-name TCP_connection_on_port_3000 --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $TCP_CONN_PORT_3000
 aws cloudwatch put-metric-data --metric-name IO_WAIT --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $IO_WAIT
